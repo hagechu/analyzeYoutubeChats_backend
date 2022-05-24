73671a085652cff865c764aabfa3914283d2b252
hagechu <hagetamachu@gmail.com>
2022-05-25 06:31:33 +0900

    Add 配信のチャットをjsonにして返せるようにしました。

--- flaskServer.py
+++ flaskServer.py
@@ -1,7 +1,33 @@
+from crypt import methods
+from urllib import response
 from flask import Flask
 from flask import request, make_response, jsonify
 from flask_cors import CORS
+from markupsafe import Markup
 from getYoutubeChats import getYoutubeChats
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["http://localhost:3000"])
+
+
+@app.route("/", methods=['GET'])
+def index():
+    html = '''
+    <p> 動いてます </p>
+    '''
+    return Markup(html)
+
+
+@app.route("/getYoutubeChats", methods=['GET', 'POST'])
+def responseYoutubeChats():
+    response = getYoutubeChats("HFi7I-Z-86E")
+
+    return make_response(jsonify(response))
+
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run(host='127.0.0.1', port=5000, use_reloader=False, threaded=False)
+
+# http://127.0.0.1:5000
+# http://127.0.0.1:5000/getYoutubeChats
--- getYoutubeChats.py
+++ getYoutubeChats.py
@@ -3,20 +3,23 @@ import time
 
 
 def getYoutubeChats(videoID):
+    chats = []
+    chatsDict = {}
     chat = pytchat.create(video_id=videoID)
-    start_time = time.perf_counter()
 
     while chat.is_alive():
         for c in chat.get().items:
-            print(f"{c.datetime} [{c.author.name}]- {c.message}")
+            # chats[chat.get().items.index(c)] = {
+            #     "date": c.datetime, "name": c.author.name, "message": c.message}
+            chats.append(
+                {"date": c.datetime, "name": c.author.name, "message": c.message})
 
-    end_time = time.perf_counter()
-    print("end")
-    elapsed_time = end_time - start_time
-    print(elapsed_time)
+    chatsDict = {"chats": chats}
 
-# dj2xt5wSA7A
-# HFi7I-Z-86E
+    return chatsDict
+
+# "dj2xt5wSA7A"
+# "HFi7I-Z-86E"
 
 
-getYoutubeChats('dj2xt5wSA7A')
+getYoutubeChats("HFi7I-Z-86E")
